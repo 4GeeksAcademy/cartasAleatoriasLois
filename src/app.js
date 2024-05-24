06bdf7bd4b399b4781edb75e4a971f5dbe5da8d5
Loisinho <159461422+loisinho87@users.noreply.github.com>
2024-05-24 11:27:48 +0000

    Cartas Aleatorias Lois

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
-/* eslint-disable */
+/* los estilos y las imágenes necesarias.*/
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+/* window.onload: evento que se ejecuta cuando todo el contenido esté cargado en el navegador. function()=función anonima */
+window.onload = function cargarPagina() {
+  // Función para generar una nueva carta
+  function generarCarta() {
+    const palos = ["corazones", "espadas", "treboles", "diamantes"]; // Pongo const en vez de Let, xke son valores fijos, no se modiican.
+    const valores = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    // Palo y un valor aleatorio
+    const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+    const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+
+    // Actualizo el contenido de la carta
+    const baraja = document.getElementById("cartaAleatoria");
+    baraja.className = `carta ${paloAleatorio}`;
+    baraja.innerHTML = `
+          <div class="top-left">${valorAleatorio}</div>
+          <div class="center">${
+            paloAleatorio === "corazones"
+              ? "♥"
+              : paloAleatorio === "espadas"
+              ? "♠"
+              : paloAleatorio === "treboles"
+              ? "♣"
+              : "♦"
+          }</div>
+          <div class="bottom-right">${valorAleatorio}</div>
+      `;
+  }
+
+  // Genero una carta al cargar la página
+  generarCarta();
 };
